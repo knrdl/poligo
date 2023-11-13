92e05edf59a1e35127d0cd5b10465a455ff7ad71
knrdl <35548889+knrdl@users.noreply.github.com>
2023-11-13 22:06:44 +0100

    chore: ci

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -it --rm -v "$PWD:$PWD" -w "$PWD" golang go build -ldflags="-s -w"
+docker run -it --rm -v "$PWD:$PWD" -w "$PWD" docker.io/golang go build -ldflags="-s -w"
 ./poligo --timeout=500ms cwd-exists warn-memory=75% term-title current-time go-version python-version nodejs-project docker-version kernel-version warn-offline shell-level virtual-env work-dir=4 sudo-root git read-only ssh-connection user-name=your_default_username exit-code=$?
--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module poligo
 
-go 1.17
+go 1.21
 
 require golang.org/x/sys v0.14.0
